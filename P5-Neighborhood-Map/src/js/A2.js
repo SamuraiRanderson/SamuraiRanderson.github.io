16081f7165fb10a65031ead9f4256e64a13616fa
Robert C Anderson <robtanderson4@gmail.com>
2015-08-22 13:57:21 -0500

    styling: Edit default loc
    feat: Api changes
    
    feat: Api changes

--- P5-Neighborhood-Map/src/js/A2.js
+++ P5-Neighborhood-Map/src/js/A2.js
@@ -3,57 +3,76 @@
 
 var Model = [
           {name: 'Jimmy\'s Food Store', address_1: '4901 Bryan St', address_2: 'Dallas, TX 75206', phone: '(214) 823-6180', lat: 32.804476, lng:  -96.773658,
-          url: 'https://www.jimmysfoodstore.com', yelpId: 'jimmys-food-store-dallas'},
+          //url: 'https://www.jimmysfoodstore.com', locationId: 122958},
+          url: 'https://www.jimmysfoodstore.com', locationId: '<img width="188px" height="188px"  src=https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e35/11325161_112942742390197_1936290244_n.jpg>',},
           {name: 'Dolly Python', address_1: '1916 N Haskell Ave', address_2: 'Dallas, TX 75204', phone: '(214) 887-3434', lat: 32.802272, lng: -96.785943,
-          url: 'http://www.dollypythonvintage.com', yelpId: 'dolly-python-dallas'},
+          //url: 'http://www.dollypythonvintage.com', locationId: 567284},
+          url: 'http://www.dollypythonvintage.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e15/11910542_1070283236315124_1142934337_n.jpg>'},
           {name: 'The Samurai Collection', address_1: '2501 N Harwood St', address_2: 'Dallas, TX 75201', phone: '(214) 965-1032', lat: 32.791917, lng: -96.806217,
-          url: 'http://www.samuraicollection.org', yelpId: 'the-ann-and-gabriel-barbier-mueller-museum-the-samurai-collection-dallas'},
+          //url: 'http://www.samuraicollection.org', locationId: 91523867},
+          url: 'http://www.samuraicollection.org', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11375767_697768100328048_1500434237_n.jpg>'},
           {name: 'Pecan Lodge', address_1: '2702 Main St', address_2: 'Dallas, TX 75226', phone: '(214) 748-8900', lat: 32.783925, lng: -96.783820,
-          url: 'http://www.pecanlodge.com', yelpId: 'pecan-lodge-dallas-3'},
+          //url: 'http://www.pecanlodge.com', locationId: 289297740},
+          url: 'http://www.pecanlodge.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/11899609_1649957018581446_854081803_n.jpg>'},
           {name: 'Smoke', address_1: '901 Fort Worth Ave', address_2: 'Dallas, TX 75208', phone: '(214) 393-4141', lat: 32.769605, lng:  -96.837312,
-          url: 'http://smokerestaurant.com', yelpId: 'smoke-dallas-2?osq=smoke'},
+          //url: 'http://smokerestaurant.com', locationId: 152607},
+          url: 'http://smokerestaurant.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/11311195_1469702123334711_1985444724_n.jpg>'},
           {name: 'Dallas Museum of Art', address_1: '1717 N Harwood St', address_2: 'Dallas, TX 75201', phone: '(214) 922-1200', lat: 32.787814, lng: -96.800793,
-          url: 'https://www.dma.org', yelpId: 'dallas-museum-of-art-dallas'},
+          //url: 'https://www.dma.org', locationId: 81723},
+          url: 'https://www.dma.org', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/11427423_957988400923986_72179099_n.jpg>'},
           {name: 'Double Wide Bar', address_1: '3510 Commerce St', address_2: 'Dallas, TX 75226', phone: '(214) 887-6510', lat: 32.784991, lng: -96.772994,
-          url: 'http://www.double-wide.com', yelpId: 'double-wide-dallas'},
-          {name: 'Torchy\'s Tacos', address_1: '5600 SMU Boulevard', address_2: 'Dallas, TX 75206', phone: '(972) 993-8226', lat: 32.842145, lng: -96.771354,
-          url: 'http://torchystacos.com/location/smu', yelpId: 'torchys-tacos-dallas-2?osq=torchy%27s+atcos+smu'},
-          {name: 'Perot Museum of Nature and Science', address_1: '3510 Commerce St', address_2: 'Dallas, TX 75226', phone: '(214) 887-6510', lat: 32.787153, lng:  -96.806644,
-          url: 'http://www.perotmuseum.org', yelpId: 'perot-museum-of-nature-and-science-dallas-2'},
+          //url: 'http://www.double-wide.com', locationId: 102177},
+          url: 'http://www.double-wide.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/11264622_1640965602788497_1475458038_n.jpg>'},
           {name: 'Luscher\'s Red Hots', address_1: '2653 Commerce St', address_2: 'Dallas, TX 75226', phone: '(214) 434-1006', lat: 32.783485, lng: -96.784181,
-          url: 'http://www.luschers.com', yelpId: 'luschers-red-hots-dallas'},
-          {name: 'Pizza Lounge', address_1: '841 Exposition Ave', address_2: 'Dallas, TX 75226', phone: '(214) 887-6900', lat: 32.782527, lng: -96.766595,
-          url: 'http://www.pizzaloungedallas.com', yelpId: 'pizza-lounge-dallas?osq=Pizza+Lounge'},
+          //url: 'http://www.luschers.com', locationId: 627073515},
+          url: 'http://www.luschers.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/11850180_1465861503717269_380158022_n.jpg>'},
+          // {name: 'Pizza Lounge', address_1: '841 Exposition Ave', address_2: 'Dallas, TX 75226', phone: '(214) 887-6900', lat: 32.782527, lng: -96.766595,
+          // url: 'http://www.pizzaloungedallas.com', locationId: 223127624},
           {name: 'Oddfellows', address_1: '316 W 7th St', address_2: 'Dallas, TX 75208', phone: '(214) 944-5958', lat: 32.748872, lng: -96.826812,
-          url: 'http://oddfellowsdallas.com', yelpId: 'oddfellows-dallas'},
+          //url: 'http://oddfellowsdallas.com', locationId: 526179323},
+          url: 'http://oddfellowsdallas.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/s320x320/e35/1517191_830642707050300_579233814_n.jpg>'},
           {name: 'We are 1976', address_1: '313 N Bishop Ave', address_2: 'Dallas, TX 75208', phone: '(214) 821-1976', lat: 32.748334, lng: -96.829321,
-          url: 'http://www.weare1976.com', yelpId: 'we-are-1976-dallas-2'},
+          //url: 'http://www.weare1976.com', locationId: 684487},
+          url: 'http://www.weare1976.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/11311495_470248963148780_588231373_n.jpg>'},
           {name: 'The Magnolia', address_1: '3699 McKinney Ave #100', address_2: 'Dallas, TX 75204', phone: '(214) 520-0394', lat: 32.808131, lng: -96.797955,
-          url: 'http://www.landmarktheatres.com/dallas/the-magnolia', yelpId: 'landmarks-magnolia-theater-dallas'},
+          //url: 'http://www.landmarktheatres.com/dallas/the-magnolia', locationId: 269612008},
+          url: 'http://www.landmarktheatres.com/dallas/the-magnolia', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e35/11330753_1613856215540275_827876991_n.jpg>'},
           {name: 'Barcadia', address_1: '1917 N Henderson Ave', address_2: 'Dallas, TX 75206', phone: '(214) 821-7300', lat: 32.811954, lng: -96.775115,
-          url: 'http://barcadiabars.com/barcadiadallas', yelpId: 'barcadia-dallas',},
+          //url: 'http://barcadiabars.com/barcadiadallas', locationId: 942082587},
+          url: 'http://barcadiabars.com/barcadiadallas', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11925841_1600702270203531_484156709_n.jpg>'},
           {name: 'The Old Monk', address_1: '2847 N Henderson Ave', address_2: 'Dallas, TX 75206', phone: '(214) 821-1880', lat: 32.820336, lng: -96.784939,
-          url: 'http://www.oldmonkdallas.com', yelpId: 'the-old-monk-dallas'},
+          //url: 'http://www.oldmonkdallas.com', locationId: 678908617},
+          url: 'http://www.oldmonkdallas.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/11261205_411164612427362_1568725969_n.jpg>'},
           {name: 'The Joule', address_1: '1530 Main St', address_2: 'Dallas, TX 75201', phone: '(214) 748-1300', lat: 32.780980, lng: -96.798352,
-          url: 'http://www.thejouledallas.com', yelpId: 'the-joule-dallas-dallas-2'},
+          //url: 'http://www.thejouledallas.com', locationId: 492431},
+          url: 'http://www.thejouledallas.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e35/11881631_985390341518588_735099582_n.jpg>' },
           {name: 'Omni Dallas Hotel', address_1: '555 S Lamar', address_2: 'Dallas, TX 75202', phone: '(214) 744-6664', lat: 32.775559, lng: -96.804319,
-          url: 'http://www.omnihotels.com/hotels/dallas', yelpId: 'omni-dallas-hotel-dallas-2'},
+          //url: 'http://www.omnihotels.com/hotels/dallas', locationId: 459169137},
+          url: 'http://www.omnihotels.com/hotels/dallas', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/11899540_411215565739641_1362445448_n.jpg>'},
           {name: 'Elbow Room', address_1: '3010 Gaston Ave', address_2: 'Dallas, TX 75226', phone: '(214) 828-9488', lat: 32.788590, lng: -96.783126,
-          url: 'http://elbowroomdallas.com', yelpId: 'the-elbow-room-dallas'}
+          //url: 'http://elbowroomdallas.com', locationId: 438168},
+          url: 'http://elbowroomdallas.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/11821143_985074391543925_2041108129_n.jpg>'},
+          {name: 'The Cedars Social', address_1: '1326 S Lamar', address_2: 'Dallas, TX 75215', phone: '(214) 928-7700', lat: 32.769119, lng: -96.795821,
+          //url: 'http://www.thecedarssocial.com', locationId: 236668314},
+          url: 'http://www.thecedarssocial.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e15/11176378_883598548348118_84931308_n.jpg>'},
+          {name: 'Lee Harvey\'s', address_1: '1807 Gould St', address_2: 'Dallas, TX 75215', phone: '(214) 428-1555', lat: 32.768950, lng: -96.789751,
+          //url: 'http://leeharveys.com', locationId: 65487}
+          url: 'http://leeharveys.com', locationId: '<img width="188px" height="188px" src=https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/11821896_1689717784591865_2029221682_n.jpg>'}
 ];
 
-var Venue = function(data) {
-     "use strict";
-     var self = this;
-     self.name = ko.observable(data.name);
-     self.address_1 = ko.observable(data.address_1);
-     self.address_2 = ko.observable(data.address_2);
-     self.phone = ko.observable(data.phone);
-     self.lat = ko.observable(data.lat);
-     self.lng = ko.observable(data.lng);
-     self.url = ko.observable(data.url);
-     self.marker = ko.observable();
-};
+// var Venue = function(data) {
+//      "use strict";
+//      var self = this;
+//      self.name = ko.observable(data.name);
+//      self.address_1 = ko.observable(data.address_1);
+//      self.address_2 = ko.observable(data.address_2);
+//      self.phone = ko.observable(data.phone);
+//      self.lat = ko.observable(data.lat);
+//      self.lng = ko.observable(data.lng);
+//      self.url = ko.observable(data.url);
+//      self.marker = ko.observable();
+//      self.locationId = ko.observable(data.locationId);
+// };
 
 var ViewModel = function () {
      "use strict";
@@ -62,12 +81,53 @@ var ViewModel = function () {
 // ** self.placeList is an empty array that is populated thr placeItem ** //
 // ** function that is pushed from the model variable array ** //
 
-     self.venueList = ko.observableArray([]);
+    self.venueList = ko.observableArray([]);
 
-     console.log(self.venueList()[i]);
 // ** Establish current marker ** //
 
-     Model.currentMarker = ko.observable();
+//     Model.currentMarker = ko.observable();
+
+// ** Instagram API ** //
+
+function loadData() {
+
+var $body = $('body');
+var $images = $('images');
+var CLIENT_ID = '6f4a01054c424c17be81b5a53bf800a0';
+var CLIENT_SECRET = '4c74e85af2394995ae5741769e25a0cf';
+var access_token = 'access_token=1962684669.6f4a010.676642d0021149d7b7d58a8cec924c0d';
+
+var instagramUrl = 'https://api.instagram.com/v1/locations/236668314/media/recent?' + access_token;
+//$body.append('<img class = "bgimg" src = "' + instagramUrl + '">');
+var instagramRequestTO = setTimeout (function(){
+ $images.text("Instagram information is not available at this time... Please try again later.");
+}, 3000);
+
+console.log(instagramUrl);
+
+$.ajax({
+     type: "Get",
+     dataType: "jsonp",
+     cache: false,
+     jsonp: "callback",
+     url: instagramUrl,
+     success: function (response) {
+          for (var i = 0; i < 1; i++){
+        $('#imageBox').append('<img src=' + response.data[i].images.low_resolution.url + ' width=200px height=200px>');
+
+console.log(response);
+         };
+
+     clearTimeout(instagramRequestTO);
+
+     }
+});
+
+return false;
+
+};
+
+$(loadData);
 
 // ** Adding the markers to the map for each location in the array. **  //
 
@@ -81,11 +141,13 @@ var ViewModel = function () {
                     address_2: Model[i].address_2,
                     phone: Model[i].phone,
                     url: Model[i].url,
+                    locationId: Model[i].locationId,
                     animation: google.maps.Animation.DROP
 
                });
 
           self.venueList.push(marker);
+
 // ** Initialize the Google Maps InfoWindow for display, when needed. ** //
 
      var infoWindow = new google.maps.InfoWindow();
@@ -100,8 +162,9 @@ var ViewModel = function () {
      function displayInfoAndBounce(m) {
      infoWindow.setContent('<h2>' + m.title + '</h2>' + '<br>' +
           m.address_1 + '<br>' + m.address_2 + '<br>' + m.phone + '</br>' +
-          '<a href=' + m.url + '>' + m.url + '</a>');
-     //setMarkersVisible(map, null);
+          '<a href=' + m.url + '>' + m.url + '</a>' + '<br>' + m.locationId + '<br>');
+          //'<img width="188px" height="188px" src=' + m.locationId + '>' + '</div>');
+          //m.locationId);
      infoWindow.open(map, m);
 
 // ** Sets the marker in motion by checking it's status and animating it if it's idle. ** //
@@ -151,8 +214,7 @@ var ViewModel = function () {
           }
 
      };
-
-     console.log(self.venueList()[i]);
+//console.log(self.venueList()[i]);
 
 // ** Add locations from array to the place list and the mobile dropdown-menu ** //
 //
@@ -162,19 +224,19 @@ var ViewModel = function () {
 
 // ** locations added to #uiList ** //
 
-          $('#uiList .locList').append('<li><a>' + Model[i].name + '<br>'
-               + Model[i].address_1 + '<br>' + Model[i].address_2 + '<br>'
-               + Model[i].phone + '<br>' + '</a></li>');
+     $('#uiList .locList').append('<li><a>' + Model[i].name + '<br>'
+          + Model[i].address_1 + '<br>' + Model[i].address_2 + '<br>'
+          + Model[i].phone + '<br>' + '<hr>' + '</a></li>');
 
 // ** 'click' binding for list ** //
 
-          $('#uiList .locList li a').last().bind('click', function(m) {
-               return function () {
-                    displayInfoAndBounce(m);
-                    stopAnimation(m);
-               };
+     $('#uiList .locList li a').last().bind('click', function(m) {
+          return function () {
+               displayInfoAndBounce(m);
+               stopAnimation(m);
+          };
 
-          }(marker));
+     }(marker));
 
 // ** Bind the click events for each map marker with the displayInfoAndBounce()function. ** //
 // ** We pass the Google Maps marker as a parameter to the function, so it knows where to move the map, ** //
