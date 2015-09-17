3f3f6531cdc0adcc71a55faaf6d65f9edd69574d
Robert C Anderson <robtanderson4@gmail.com>
2015-09-16 23:48:43 -0500

    init: Add current Neighborhood Map project

--- Resume/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp')
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename'),
-	minifyCSS = require('gulp-minfy-css');
-
-gulp.task('scripts', function() {
-	gulp.src('js/')
-})
